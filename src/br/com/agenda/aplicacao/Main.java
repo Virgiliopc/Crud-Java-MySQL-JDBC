1699904745b54ed46ea8d19e4b7212a96ddb67a5
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-25 22:40:06 -0300

    Create Main.java
    Create Main.java

---
+++ src/br/com/agenda/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package br.com.agenda.aplicacao;
+
+import br.com.agenda.model.Contato;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Contato contato = new Contato();
+		contato.setNome("Maria Gabriela");
+		contato.setId(55);
+		contato.setDataCadastro("22/05/2023");
+	}
+
+}
